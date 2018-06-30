4b66b57090d7826ec9636e41e650df3b15e29a03
yuhq08 <642406378@qq.com>
2018-06-30 20:55:54 +0800

    630

--- app/config.php
+++ app/config.php
@@ -161,7 +161,7 @@ $configs = [
         // 缓存保存目录
         'path'   => CACHE_PATH,
         // 缓存前缀
-        'prefix' => '',
+        'prefix' => 'tx',
         // 缓存有效期 0表示永久缓存
         'expire' => 0,
     ],
